219d76b2ec2ad2c45cbf072d5afe1616b21f2809
fabiha15 <fabihakhan1999@gmail.com>
2025-10-01 11:18:02 +0600

    cart section done

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -33,19 +33,91 @@ const Hero = () => {
               </div>
             </Button>
           </div>
-          <div className="grid mt-[26px] gap-[13.56px]">
+          <div className="grid mt-[26px] gap-[13.56px] ">
             {cartData &&
               cartData.map((item: Product) => {
                 return (
                   <div key={item.id} className="flex items-center gap-[15px]">
                     <div className="w-[124px] h-[124px] p-[7px] bg-black-50 rounded-md">
                       <div className="h-[110px] w-[110px] relative">
-                        <Image
-                         
-                          src={item.image}
-                          alt="product"
-                          fill
-                        />
+                        <Image src={item.image} alt="product" fill />
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-3">
+                      <p className="text-lg font-normal text-black">
+                        {item.name}
+                      </p>
+                      <p className="text-base font-normal text-white-900">
+                        {item.description}
+                      </p>
+                      <div className="flex items-center gap-2">
+                        <p className="text-lg font-semibold text-orange-500">
+                          {item.prevPrice}
+                        </p>
+                        <p className="text-xs font-semibold text-white-800">
+                          {item.newPrice}
+                        </p>
+                      </div>
+                      <div className="flex items-center justify-between">
+                        <div className="flex items-center gap-[18px]">
+                          <p className="text-base font-normal text-white-900">
+                            QTY :
+                          </p>
+                          <div className="flex items-center justify-between w-[108px] p-1 bg-black-50 rounded-full">
+                            <div className="w-[19px] h-[18px] flex items-center justify-center bg-black  rounded-full">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="11"
+                                height="2"
+                                viewBox="0 0 11 2"
+                                fill="none"
+                              >
+                                <path
+                                  d="M0.791748 0.864075H9.63104"
+                                  stroke="white"
+                                  strokeWidth="0.777778"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                              </svg>
+                            </div>
+                            <p className="text-base font-normal text-gray-500">
+                              {item.quantity}
+                            </p>
+                             <div className="w-[19px] h-[18px] flex items-center justify-center bg-black  rounded-full">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="11"
+                                height="2"
+                                viewBox="0 0 11 2"
+                                fill="none"
+                              >
+                                <path
+                                  d="M0.791748 0.864075H9.63104"
+                                  stroke="white"
+                                  strokeWidth="0.777778"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                              </svg>
+                            </div>
+                          </div>
+                        </div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="25"
+                          height="25"
+                          viewBox="0 0 25 25"
+                          fill="none"
+                        >
+                          <path
+                            d="M3.82251 6.86407H5.82251M5.82251 6.86407H21.8225M5.82251 6.86407V20.8641C5.82251 21.3945 6.03322 21.9032 6.4083 22.2783C6.78337 22.6534 7.29208 22.8641 7.82251 22.8641H17.8225C18.3529 22.8641 18.8617 22.6534 19.2367 22.2783C19.6118 21.9032 19.8225 21.3945 19.8225 20.8641V6.86407H5.82251ZM8.82251 6.86407V4.86407C8.82251 4.33364 9.03322 3.82493 9.4083 3.44986C9.78337 3.07479 10.2921 2.86407 10.8225 2.86407H14.8225C15.3529 2.86407 15.8617 3.07479 16.2367 3.44986C16.6118 3.82493 16.8225 4.33364 16.8225 4.86407V6.86407M10.8225 11.8641V17.8641M14.8225 11.8641V17.8641"
+                            stroke="#6B6B6B"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
                       </div>
                     </div>
                   </div>
