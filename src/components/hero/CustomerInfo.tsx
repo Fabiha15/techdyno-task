1a20dc10f6b4cd976dd15e51deb563c1acd9b00c
fabiha15 <fabihakhan1999@gmail.com>
2025-10-01 12:48:27 +0600

    customer info done

---
+++ src/components/hero/CustomerInfo.tsx
@@ -0,0 +1,53 @@
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+const CustomerInfo = () => {
+  return (
+    <div className="flex flex-col gap-[26.86px] rounded-md pt-9 pb-[18px] px-[18px] border border-black-50 ">
+      <p className="text-2xl font-medium text-black">Customer Information</p>
+      <div>
+        <p className="text-base font-medium text-gray-400 mb-[11.3px]">
+          Your Name*
+        </p>
+        <Input
+          className="text-sm font-normal border border-gray-300 rounded-sm"
+          placeholder="aman***"
+        />
+      </div>
+      <div>
+        <p className="text-base font-medium text-gray-400 mb-[11.3px]">
+          Mobile*
+        </p>
+        <Input
+          className="text-sm font-normal border border-gray-300 rounded-sm"
+          placeholder="019*******"
+        />
+      </div>
+      <div>
+        <p className="text-base font-medium text-gray-400 mb-[11.3px]">
+          E-mail ( Optional )*
+        </p>
+        <Input
+          className="text-sm font-normal border border-gray-300 rounded-sm"
+          placeholder="@email.****"
+        />
+      </div>
+      <div>
+        <p className="text-base font-medium text-gray-400 mb-[11.3px]">
+          Address*
+        </p>
+        <Input
+          className="text-sm font-normal border border-gray-300 rounded-sm"
+          placeholder="Suggested location"
+        />
+      </div>
+      <div className="flex justify-end items-center">
+        <Button className="w-[90px]   bg-orange-500  text-white  rounded-full">
+          Save
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerInfo;
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,19 +1,18 @@
-import { Cart } from "./Cart"
-
-
-
-
-
+import { Cart } from "./Cart";
+import CustomerInfo from "./CustomerInfo";
 
 const Hero = () => {
   return (
-   <div className="content-wrapper mt-[56px] mb-[75px] ">
+    <div className="content-wrapper mt-[56px] mb-[75px] ">
       <p className="text-4xl font-semibold text-black">Shopping Cart</p>
       <div className="flex mt-[26px] gap-[10px] ">
-      <Cart />
+        <Cart />
+        <div className="flex flex-col w-[588px]  ">
+          <CustomerInfo />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
